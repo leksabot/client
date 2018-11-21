d0286b6bebb33cfbcc2a597a071eba9467a7510e
ismail <mismailyusuf@gmail.com>
2018-11-21 17:12:41 +0700

    add offline notice && remove quiz timeout bug

--- components/OfflineNotice.js
+++ components/OfflineNotice.js
@@ -3,10 +3,40 @@ import { View, Text, NetInfo, Dimensions, StyleSheet } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 class OfflineNotice extends Component {
+
+  state = {
+    isConnected: true
+  }
+
+  componentDidMount() {
+    NetInfo.getConnectionInfo()
+    .then(info => {
+      this.handleConnectivityChange(info.type)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  handleConnectivityChange(conType) {
+    if (conType !== 'none') {
+      this.setState({
+        isConnected: true
+      })
+    } else {
+      this.setState({
+        isConnected: false
+      })
+    }
+  }
+
   render() {
     return (
+      !this.state.isConnected &&
       <View style={styles.offlineContainer}>
-        <Text style={styles.offlineText}>No Internet Connection</Text>
+        <View style={{width: '70%'}}>
+          <Text style={styles.offlineText}>Sorry, but it seems that you're not connected to the internet.</Text>
+        </View>
       </View>
     )
   }
@@ -17,12 +47,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#ff3f40',
     justifyContent: 'center',
     alignItems: 'center',
-    width,
-    height,
-    position: 'absolute'
+    width: width,
+    height: height,
+    position: 'absolute',
+    elevation: 100
   },
   offlineText: { 
-    color: '#fff'
+    color: '#fff',
+    textAlign: "center"
   }
 });
 
